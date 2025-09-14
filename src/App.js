469f8f01a0401125e6cd6e67bd4c42fd653481c7
Thakur-Manish <useremail : "chfan93726@gmail.com>
2025-09-14 18:46:09 +0530

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/Home";
-// import Wishlist from "./context/WishlistContext"; 
 import Watchlist from "./pages/Watchlist";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,14 +10,17 @@ import MovieDetails from "./pages/MovieDetails";
 function App() {
   return (
     <>
-    <Header  />
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Watchlist" element={<Watchlist />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-      </Routes>
-    <Footer />
-    <ToastContainer position="bottom-right" autoClose={2000} />
+      <Header />
+      <Router basename="/moviewatchlist">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+
+      <Footer />
+      <ToastContainer position="bottom-right" autoClose={2000} />
     </>
   );
 }
